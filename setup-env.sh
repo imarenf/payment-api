10b841d1071b46a0d1ed8515328b371ae18dd26a
imarenf <asap.illidan@gmail.com>
2022-11-22 19:26:12 +0300

    Fixed python version in setup-env script

--- setup-env.sh
+++ setup-env.sh
@@ -2,26 +2,26 @@
 
 sudo apt-get install -y python3.10-venv
 
-in_venv=$(python -c 'import sys; return sys.prefix != sys.base_prefix')
+in_venv=$(python3 -c 'import sys; return sys.prefix != sys.base_prefix')
 
 if ! ${in_venv}; then
-  python -m venv venv
+  python3 -m venv venv
   source venv/bin/activate
 fi
 
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 cd src
 
-python manage.py makemigrations payment --no-input -v 0
+python3 manage.py makemigrations payment --no-input -v 0
 
-python manage.py migrate --no-input
+python3 manage.py migrate --no-input
 
 if [ "${DJANGO_SUPERUSER_NAME}" ]; then
-  python manage.py create_admin \
+  python3 manage.py create_admin \
           --username "${DJANGO_SUPERUSER_USERNAME}" \
           --email "${DJANGO_SUPERUSER_EMAIL}" \
           --password "${DJANGO_SUPERUSER_PASSWORD}"
 fi
 
-python manage.py fill_database --count 10
+python3 manage.py fill_database --count 10
